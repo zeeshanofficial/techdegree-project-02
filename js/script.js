b0c87f0636abfb29bf4835e5bfb1431fcc3efbc2
zeeshanofficial <34814964+zeeshanofficial@users.noreply.github.com>
2019-04-30 12:02:02 +0530

    fixed errors in the appendPageLinks function and completed the project

--- js/script.js
+++ js/script.js
@@ -3,38 +3,11 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//Declaring global variables
 const listItems = document.querySelectorAll('.student-item');
 const perPage = 10;
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//Function to hide all students and show only a particular set of ten
 const showPage = (list, page) => {
   let startIndex = (page * perPage) - perPage;
   let endIndex = page * perPage;
@@ -45,55 +18,45 @@ const showPage = (list, page) => {
       list[i].style.display='none';
     }
   }
-  
 }
 
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
+//Function to create pagination buttons, adds them to the DOM, and add their functionality.
 
 const appendPageLinks = (list) => {
-
   const pages = Math.ceil(list.length/perPage);
-
   const pageContainer = document.querySelector('.page');
   const pageDiv = document.createElement('div');
+  pageDiv.classList.add('pagination');
   const ul = document.createElement('ul');
-  const li = document.createElement('li');
-  const a = document.createElement('a');
+  pageDiv.appendChild(ul);
+  pageContainer.appendChild(pageDiv);
 
-  for(let i= 0; i < pages.length; i+= 1){
-      a.href = '#';
-      a[i].textContent = i+1;
-      pageDiv.className = 'pagination';
+  //Button elements are created dynamically to the DOM 
+  for(let i= 0; i < pages; i+= 1){
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.setAttribute('href','#');
+      a.textContent = i+1;
       li.appendChild(a);
       ul.appendChild(li);
-      pageDiv.appendChild(ul);
-      pageContainer.appendChild(pageDiv);
   }
 
-  const aLinks = document.querySelectorAll('a');
-  aLinks[0].className = 'active';
+//a variable is created for setting and removing 'active' classes on the button(s)
+  const aLinks = ul.querySelectorAll('a');
+  aLinks[0].setAttribute('class','active');
 
-  ul.addEventListener('click', (e) => {
-    for(let i=0; i<aLinks.length; i +=1) {
-      aLinks[i].className = '';
-    }
-    e.target.className = 'active';
-    const clickedListNumber = e.target.textContent;
-    showPage(listItems, clickedListNumber);
-  });
-    
+  //Buttons are made functional by looping through all a tag and adding click events
+  for(let i=0; i<aLinks.length; i+=1){  
+    ul.addEventListener('click', (e) =>{
+      showPage(listItems, e.target.textContent);  
+      aLinks[i].removeAttribute('class');
+      e.target.setAttribute('class','active');
+    })
+  }
 };
 
+//And finally the function call
 showPage(listItems, 1);
 appendPageLinks(listItems);
 
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
 
